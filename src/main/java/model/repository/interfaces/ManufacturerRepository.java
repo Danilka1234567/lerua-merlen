6a762427287a692f8dcb4d4905f7207ca8460677
Danilka1234567 <checkh_mail2@mail.ru>
2026-07-12 00:38:50 +0300

    small changes in manufacturer repository interface

--- src/main/java/model/repository/interfaces/ManufacturerRepository.java
+++ src/main/java/model/repository/interfaces/ManufacturerRepository.java
@@ -2,13 +2,17 @@ package model.repository.interfaces;
 
 import model.entities.Manufacturer;
 import model.valueobjects.Address;
+import model.valueobjects.Email;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ManufacturerRepository extends CrudRepository<Manufacturer>{
 
     List<Manufacturer> findAllBySpecialization(String specialization);
-    List<Manufacturer> findAllByAddress(Address address);
+    Optional<Manufacturer> findByAddress(Address address);
     boolean existsByAddress(Address address);
+    Optional<Manufacturer> findByEmail(Email email);
+    boolean existsByEmail(Email email);
 
 }
