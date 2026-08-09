d680d2ac5112d4155143f9c7781225c78f603932
Danilka1234567 <checkh_mail2@mail.ru>
2026-08-09 03:58:46 +0300

    created ConnectionManager class

---
+++ src/main/java/infrastracture/config/ConnectionManager.java
@@ -0,0 +1,41 @@
+package infrastracture.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+
+    private static final String URL;
+    private static final String USERNAME;
+    private static final String PASSWORD;
+    private static final Connection currentConn;
+
+    static{
+
+        try{
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Can't find postgre sql driver!", e);
+        }
+
+
+        Dotenv dotenv = Dotenv.load();
+        URL = "jdbc:postgresql://" + dotenv.get("DB_HOST") + ":" + dotenv.get("DB_PORT") + dotenv.get("DB_NAME");
+        USERNAME = dotenv.get("DB_USER");
+        PASSWORD = dotenv.get("DB_PASSWORD");
+
+        try{
+            currentConn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can't create connection with DB", e);
+        }
+    }
+
+    public Connection getConnection() throws SQLException{
+        currentConn.setAutoCommit(true);
+        return currentConn;
+    }
+}
