368bf3ac7057a8387c2a42a953b328c648721638
Danilka1234567 <checkh_mail2@mail.ru>
2026-08-12 05:18:39 +0300

    added db init class

---
+++ src/main/java/infrastructure/config/DbInitializer.java
@@ -0,0 +1,31 @@
+package infrastructure.config;
+
+import infrastructure.utils.ResourceReader;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbInitializer {
+
+    public static void init(){
+
+        Connection conn = ConnectionManager.getConnection();
+
+
+        try(Statement statement = conn.createStatement();){
+
+            String directory = "sql/init/";
+            statement.execute(ResourceReader.read(directory + "create_manufacturer.sql"));
+            statement.execute(ResourceReader.read(directory + "create_warehouse.sql"));
+            statement.execute(ResourceReader.read(directory + "create_user.sql"));
+            statement.execute(ResourceReader.read(directory + "create_session.sql"));
+            statement.execute(ResourceReader.read(directory + "create_product.sql"));
+            statement.execute(ResourceReader.read(directory + "create_order.sql"));
+
+        }catch (SQLException e){
+            throw new RuntimeException("Failed to init DB!", e);
+        }
+    }
+
+}
