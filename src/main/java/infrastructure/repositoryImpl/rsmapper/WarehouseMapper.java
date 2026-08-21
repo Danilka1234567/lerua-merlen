e99f0df0a1eabf7ef2e08732cb34cc0f4a7a619c
Danilka1234567 <checkh_mail2@mail.ru>
2026-08-22 01:21:05 +0300

    added warehouse mapper

---
+++ src/main/java/infrastructure/repositoryImpl/rsmapper/WarehouseMapper.java
@@ -0,0 +1,30 @@
+package infrastructure.repositoryImpl.rsmapper;
+
+import model.entity.Warehouse;
+import model.vo.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WarehouseMapper implements RsMapper<Warehouse> {
+
+    @Override
+    public Warehouse mapRsToEntity(ResultSet rs) throws SQLException {
+        return new Warehouse(
+                rs.getLong("id"),
+                rs.getBoolean("is_deleted"),
+                rs.getDate("registration_date").toLocalDate(),
+                new ContactInfo(
+                        new PhoneNumber(rs.getString("phone_number")),
+                        new Email(rs.getString("email"))
+                ),
+                new FullAddress(
+                        rs.getString("country"),
+                        rs.getString("region"),
+                        rs.getString("city"),
+                        new StreetAddress(rs.getString("street_address"))
+                ),
+                rs.getInt("capacity")
+        );
+    }
+}
