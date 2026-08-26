6ac761c431b71a2ca9bae341225eee049220c195
Danilka1234567 <checkh_mail2@mail.ru>
2026-08-26 19:14:29 +0300

    finished service layer

--- src/main/java/model/service/ManufacturerService.java
+++ src/main/java/model/service/ManufacturerService.java
@@ -2,17 +2,15 @@ package model.service;
 
 import infrastructure.config.ConnectionManager;
 import model.entity.Manufacturer;
-import model.exception.ForeignKeyViolationException;
-import model.exception.RepositoryException;
+import model.exception.EntityNotFoundException;
 import model.exception.ServiceException;
-import model.exception.UniqueViolationException;
 import model.repository.ManufacturerRepository;
 import model.service.shared.Transaction;
 import model.service.shared.Validator;
-import model.vo.ContactInfo;
-import model.vo.Id;
+import model.vo.*;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class ManufacturerService {
 
@@ -46,5 +44,77 @@ public class ManufacturerService {
         });
     }
 
+    public void markAsDeleted(Id id){
+        Validator.validateNotNull(id, "Id");
+        int affectedRows = manufacturerRepository.setDeletionStatus(false,
+                 id, ConnectionManager.getConnectionSingletone());
+        if (affectedRows == 0)
+            throw new ServiceException("Can't mark manufacturer as deleted");
+    }
 
+    public int cleanDeleted(){
+        return manufacturerRepository.remove(ConnectionManager.getConnectionSingletone());
+    }
+
+    public List<Manufacturer> getAllDeleted(){
+        return manufacturerRepository.findAllByDeleteStatus(true, ConnectionManager.getConnectionSingletone());
+    }
+
+    public Manufacturer getById(Id id){
+        Validator.validateNotNull(id, "Manufacturer id");
+        return manufacturerRepository.findById(
+                id, ConnectionManager.getConnectionSingletone()
+        ).orElseThrow(
+                () -> new EntityNotFoundException("Unknown manufacturer id")
+        );
+    }
+
+    public Manufacturer getByPhoneNumber(PhoneNumber phoneNumber){
+        Validator.validateNotNull(phoneNumber, "Phone number");
+        return manufacturerRepository.findByPhoneNumber(
+                phoneNumber, ConnectionManager.getConnectionSingletone()
+        ).orElseThrow(
+                () -> new EntityNotFoundException("Unknown manufacturer phone number")
+        );
+    }
+
+    public Manufacturer getByEmail(Email email){
+        Validator.validateNotNull(email, "Email");
+        return manufacturerRepository.findByEmail(
+                email, ConnectionManager.getConnectionSingletone()
+        ).orElseThrow(
+                () -> new EntityNotFoundException("Unknown manufacturer email")
+        );
+    }
+
+    public List<Manufacturer> getAllByCity(String city){
+        Validator.validateNotNull(city, "City");
+        return manufacturerRepository.findAllByCity(city, ConnectionManager.getConnectionSingletone());
+    }
+
+    public List<Manufacturer> getAllByCountry(String country){
+        Validator.validateNotNull(country, "Country");
+        return manufacturerRepository.findAllByCountry(country, ConnectionManager.getConnectionSingletone());
+    }
+
+    public List<Manufacturer> getAllByRegion(String region){
+        Validator.validateNotNull(region, "Region");
+        return manufacturerRepository.findAllByRegion(region, ConnectionManager.getConnectionSingletone());
+    }
+
+    public Manufacturer getByFullAddress(FullAddress fullAddress){
+        Validator.validateNotNull(fullAddress, "Full address");
+        return manufacturerRepository.findByFullAddress(
+                fullAddress, ConnectionManager.getConnectionSingletone()
+        ).orElseThrow(
+                () -> new EntityNotFoundException("Unknown manufacturer full address")
+        );
+    }
+
+    public List<Manufacturer> getAllBySpecialization(String specialization){
+        Validator.validateNotNull(specialization, "Specialization");
+        return manufacturerRepository.findAllBySpecialization(
+                specialization, ConnectionManager.getConnectionSingletone()
+        );
+    }
 }
